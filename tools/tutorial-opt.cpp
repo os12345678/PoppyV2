3dccd3bc81edbcf0c2bc095ef5eed716f5e73ae1
Oliver Salman <oliversalman1@gmail.com>
2024-05-20 19:12:22 +1000

    Using Tablegen for Passes

--- tools/tutorial-opt.cpp
+++ tools/tutorial-opt.cpp
@@ -1,6 +1,6 @@
-#include "lib/Transform/Affine/AffineFullUnroll.h"
+#include "lib/Transform/Affine/Passes.h"
+#include "lib/Transform/Arith/Passes.h"
 #include "mlir/include/mlir/InitAllDialects.h"
-#include "lib/Transform/Arith/MulToAdd.h"
 #include "mlir/include/mlir/Pass/PassManager.h"
 #include "mlir/include/mlir/Pass/PassRegistry.h"
 #include "mlir/include/mlir/Tools/mlir-opt/MlirOptMain.h"
@@ -9,10 +9,8 @@ int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
   mlir::registerAllDialects(registry);
 
-  mlir::PassRegistration<mlir::tutorial::AffineFullUnrollPass>();
-  mlir::PassRegistration<mlir::tutorial::AffineFullUnrollPassAsPatternRewrite>();
-  mlir::PassRegistration<mlir::tutorial::MulToAddPass>();
-
+  mlir::tutorial::registerAffinePasses();
+  mlir::tutorial::registerArithPasses();
 
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "Tutorial Pass Driver", registry));
